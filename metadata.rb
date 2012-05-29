40c0d4902afdf3bd7f2d24ef9cc7fa4da08c19bb
fak3r <fak3r@fak3r.com>
2012-05-29 13:00:43 -0500

    updated

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Phil Cryer"
 maintainer_email "pcryer@mbl.edu"
-license          "Apache 2.0"
-description      "Installs/Configures virtuoso-cookbook"
+license          "Apache v2.0"
+description      "Installs and configures virtuoso-cookbook in Ubuntu Linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
